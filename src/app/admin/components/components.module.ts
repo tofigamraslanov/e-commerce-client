10e0a6ff07421fd2e12173520b87ebb4db9552d5
Tofiq Əmraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2022-05-15 20:16:41 +0400

    🚀 Development of Admin and UI Infrastruture

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrdersModule,
+    CustomersModule,
+    DashboardModule
+  ]
+})
+
+export class ComponentsModule { }
---
+++ src/app/admin/components/customers/customers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+  declarations: [
+    CustomersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+export class CustomersModule { }
